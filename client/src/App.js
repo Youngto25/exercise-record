dce84bb0dbe083ed89226b1d36f336a1ca2a3785
Blake <2979337714@qq.com>
2020-07-04 11:59:06 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.less";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="test">how are you</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
